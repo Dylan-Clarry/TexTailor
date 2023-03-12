5ac7fe8a34aea7744430e86a4252d11a0c93f362
Dylan <dylan.imail@gmail.com>
2023-03-11 21:32:43 -0500

    Login button on page working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
 // src/pages/index.tsx
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home = () => {
+  const { data: session, status } = useSession();
+  if(status == 'loading') {
+      return <main>Loading...</main>
+  }
+
   return (
     <main>
       <h1>Tex Tailor</h1>
+      <div>
+        {session ? (
+          <>
+            <p>hi {session.user?.name}</p>
+            <button
+              onClick={() => {
+                signOut().catch(console.log);
+              }}
+            >
+              Logout
+            </button>
+          </>
+        ) : (
+          <button
+            onClick={() => {
+              signIn("discord").catch(console.log);
+            }}
+          >
+            Login with Discord
+          </button>
+        )}
+      </div>
     </main>
   );
 };
